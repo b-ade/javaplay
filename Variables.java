ff2dc550d95b8b20cd0f4981b40d3f8799a772f1
0luwadamilola <dakinseesin@gmail.com>
2021-01-24 23:15:32 +0000

    Introduction to data declaration
    //variable declaration and initialization
    //arithmetic operation
    //print statement
    //Concatenation

---
+++ Variables.java
@@ -0,0 +1,30 @@
+public class Variables {
+    public static void main(String[] args) {
+// char - character . e.g  'a', 'b'
+//         byte - 8 bits, numeric value, ranging from -128 to 127 
+//         boolean - 8 bits or a  byte,
+//         short - 16 bits; numeric values
+//         int - 32 bits; numeric values
+//         long - 64 bits; number
+//         float - 32 bits; decimal values
+//         double - 64 bits; decimal values 
+ 
+// create two variables to hold two integers
+// create a variable to hold the sum of the two integers previously created
+// print out the sum in the format of a+b = c
+
+
+// <type of data> <name of variable> [= <value to be assigned>];
+        int age = 20;
+        int car = 30;
+        int sum = age + car;
+        //  System.out.println("My age is "+age);
+        System.out.println("The two things are"+sum);
+         System.out.println("The two things are "+sum+"!");
+         System.out.println(""+age+"+"+car+"="+sum);
+//variable declaration and initialization
+//arithmetic operation
+//print statement
+//Concatenation
+}
+}
